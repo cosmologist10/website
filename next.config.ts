81081e6b5e17f88ae4f71d731fdbb5e4a6979137
shweta suman <shwetasuman@shwetas-MBP.lan>
2025-10-29 11:48:39 +0530

    fix: basepath after adding custom domain

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
-  basePath: "/website",
-  assetPrefix: "/website",
+  basePath: "",
+  assetPrefix: "",
 };
 
 export default nextConfig;
